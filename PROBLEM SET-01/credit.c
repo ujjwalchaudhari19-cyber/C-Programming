8ae6f96cfc6ffd315365ee9720dddd0dfa0ebf4d
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-06-23 17:25:47 +0530

    Checked if the credit card exists or not using luhn's algorithm

--- PROBLEM SET-01/credit.c
+++ PROBLEM SET-01/credit.c
@@ -1,8 +1,65 @@
 #include <stdio.h>
 
 int main(void){
-  long int cardnumber[];
-  scanf("%li", &cardnumber);
-  printf("%li\n", cardnumber);
+  long cardnumber;
+  long check;
+  int digit;
+  int splitdigit; 
+  int checksum=0;
+  int pos = 1;
+  int length = 0;
+
+  printf("Give your card number: ");
+  scanf("%li", &cardnumber); 
+  
+  check = cardnumber;
+  while(cardnumber>0){
+    digit = cardnumber%10;
+    cardnumber = cardnumber/10;
+    
+    if(pos%2==0){
+      digit = digit*2;
+
+      if(digit>9){
+        splitdigit = digit%10;
+        digit = digit/10;
+        checksum = checksum + splitdigit + digit;
+      }
+
+      else{
+        checksum = checksum + digit;
+      }
+    }
+
+    else{
+      checksum = checksum+digit;
+    }
+
+    length++;
+    pos++;
+  }
+
+  if(checksum%10==0){
+    printf("Card is legit\n");
+    while(check>=100){
+      check /= 10;
+    }
+
+    if((check == 34 || check == 37) && (length == 15)){
+      printf("It is an american express!!\n");
+    }
+
+    else if((check == 51 || check == 52 || check == 53 || check ==54 || check == 55) && (length== 16)){
+      printf("This is a master card\n");
+    }
+
+    else if((check/10 == 4) && (length ==13 || length == 16)){
+      printf("Its a visa card\n");
+    }
+  }
+
+  else{
+    printf("Card is Fake\n");
+  }
 
 }
