bb639e6707c12803f4750f083313ece8029653d2
ujjwalchaudhari19-cyber <ujjwalchaudhari19@gmail.com>
2026-06-26 23:02:37 +0530

    Tried to encrypt a plain eng text using caesar's method

--- PROBLEM SET-02/caesar.c
+++ PROBLEM SET-02/caesar.c
@@ -1,45 +1,60 @@
-  #include <stdio.h>
-  #include <ctype.h>
-  #include <string.h>
-  #include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
-  int main(int argc, char *argv[]){
-    int i=0;
-    char plain_text[10000];
-    if(argc != 2)
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
-    }
+void encrypt(char plain_text[], int k);
 
-    while(argv[1][i]!='\0'){
-      if (!(isdigit(argv[1][i]))){
-        printf("Usage: ./caesar key\n");
-        return 1;
-      }
-      i++;
-    }
+int main(int argc, char *argv[])
+{
+  int i = 0;
+  char plain_text[10000];
+  char cypher_text[10000];
 
-    int k = atoi(argv[1]);
-    
-    printf("Plain text: ");
-    fgets(plain_text, 10000, stdin);
+  if (argc != 2)
+  {
+    printf("Usage: ./caesar key\n");
+    return 1;
   }
 
-int encrypt(char plain_text[], int k){
-  int i =0;
-  char cypher_text[10000];
-  while(plain_text[i] !='\0'){
-    if(isupper(plain_text[i])){
-      ((plain_text[i] - 'A' + k) % 26) + 'A';
+  while (argv[1][i] != '\0')
+  {
+    if (!(isdigit(argv[1][i])))
+    {
+      printf("Usage: ./caesar key\n");
+      return 1;
+    }
+    i++;
+  }
+
+  int k = atoi(argv[1]);
+
+  printf("Plain text: ");
+  fgets(plain_text, 10000, stdin);
+
+  printf("Cypher text: ");
+  encrypt(plain_text, k);
+  return 0;
+}
+
+void encrypt(char plain_text[], int k)
+{
+  int i = 0;
+  while (plain_text[i] != '\0')
+  {
+    if (isupper(plain_text[i]))
+    {
+      printf("%c", ((plain_text[i] - 'A' + k) % 26) + 'A');
     }
 
-    else if(islower(plain_text[i])){
-      ((plain_text[i] - 'a' + k) % 26) + 'a';
+    else if (islower(plain_text[i]))
+    {
+      printf("%c", ((plain_text[i] - 'a' + k) % 26) + 'a');
     }
 
-    else{
-      continue;
+    else
+    {
+      printf("%c", plain_text[i]);
     }
     i++;
   }
